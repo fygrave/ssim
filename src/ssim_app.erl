545d33f6091a11c5cfc6d0dd7c17b566baa08f67
Fyodor Y <fygrave@gmail.com>
2011-08-08 16:26:02 +0800

    Initial commit

---
+++ src/ssim_app.erl
@@ -0,0 +1,16 @@
+-module(ssim_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    ssim_sup:start_link().
+
+stop(_State) ->
+    ok.
