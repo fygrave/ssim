693a3bb48327d8ed388f9f2407ad2ca52259bf5e
Fyodor Y <fygrave@gmail.com>
2011-08-11 16:46:05 +0800

    Importing app description and app

--- src/ssim_app.erl
+++ src/ssim_app.erl
@@ -1,3 +1,11 @@
+%%%-------------------------------------------------------------------
+%%% @author fygrave <fygrave@o0o.nu>
+%%% @copyright (C) 2011, fyodor yarochkin
+%%% @doc
+%%%  SSIM Application: OSSIM Server erlang alternative
+%%% @end
+%%% Created : 16 Mar 2010 by fyodor yarochkin <fygrave@o0o.nu>
+%%%----------------------------------------------------------------
 -module(ssim_app).
 
 -behaviour(application).
@@ -10,7 +18,13 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    ssim_sup:start_link().
+    
+    case ssim_sup:start_link() of
+        {ok, Pid} ->
+            {ok, Pid};
+	Error -> 
+	    Error
+    end.
 
 stop(_State) ->
     ok.
